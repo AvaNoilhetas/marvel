3522010e649fb4692403409f1176b45515c149ba
AvaNoilhetas <a.noilhetas@hotmail.fr>
2021-02-03 23:38:47 +0100

    [+]: Start project

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    maxHeight: {
+      offer: "50rem"
+    },
+    container: {
+      center: true,
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1280px",
+        xl: "1280px"
+      },
+      padding: {
+        sm: "2rem",
+        lg: "2rem",
+        xl: "5rem",
+        "2xl": "6rem",
+        DEFAULT: "1rem"
+      }
+    },
+    colors: {
+      primary: "#09b1ba",
+      lightPrimary: "#75d0d6",
+      secondary: "#999",
+      red: "#ef4444",
+      white: "#ffffff",
+      gray: "#ebedee",
+      lightGray: "#f5f6f7",
+      default: "#666666",
+      dark: "#111111",
+      transparent: "transparent"
+    },
+    fontFamily: {
+      body: ['"Maison Neue"', "Helvetica Neue", "Helvetica-Neue", "Arial"]
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [require("@tailwindcss/forms")]
+};
