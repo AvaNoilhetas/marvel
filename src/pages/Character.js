836d5aa18bc198be6f5c98bacb1aa487247e0c49
AvaNoilhetas <a.noilhetas@hotmail.fr>
2021-02-04 01:45:05 +0100

    [+]: Add character page

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,7 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "./../components/Loader";
 
 const Character = () => {
-  return <div>Character</div>;
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://marvel-app-back.herokuapp.com/character/${id}`
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [id]);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && <div>Character</div>}
+    </>
+  );
 };
 
 export default Character;
